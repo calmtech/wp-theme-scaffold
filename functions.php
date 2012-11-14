c5dfdd917655e76b3ba8cca8db8f35dd1e565bfc
mkogawa34@gmail.com <info@calmtech.net>
2012-11-15 06:40:06 +0900

    Initial commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ...
+ *
+ * @package WordPress
+ * @subpackage ...
+ */
+//メニュー機能の有効化
+add_theme_support( 'menus' );
+//アイキャッチの有効化
+add_theme_support( 'post-thumbnails' );
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * メインテンプレート
+ *
+ * @package WordPress
+ * @subpackage ...
+ */
+?>
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>  
+    <?php while (have_posts()) : the_post(); ?>  
+        <?php the_content('Read the rest of this entry »'); ?>  
+    <?php endwhile; ?>  
+    <?php else : ?>  
+    //Something that happens when a post isn’t found.  
+<?php endif; ?>  
+
+<?php get_footer(); ?>
